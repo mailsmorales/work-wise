960d16388acc0a000a463c394e18593e580d525d
mukhamed-Ali <mailsmorales21@mail.ru>
2022-07-23 15:19:07 +0600

    new-file-header-posts-firebase-helpers-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import { Header } from './components/Header/Header'
 import { Home } from './pages/Home/Home'
 
---
+++ src/helpers/date.js
@@ -0,0 +1,6 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ru } from "date-fns/locale";
+
+
+export const dueDate = (date, lang = ru) => format(date, "EEEE d MMMM y", { locale: lang })
+export const toNow = (date, lang = ru) => formatDistanceToNow(date, { locale: lang })
