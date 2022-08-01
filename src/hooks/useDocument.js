245782355fbf832903b42267830e0d4b0bb4e735
mukhamed-Ali <mailsmorales21@mail.ru>
2022-08-01 17:11:36 +0600

    useDocument-getDocument

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,28 +1,45 @@
-import { doc, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, doc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { firestore } from "../firebase/config";
 
-export const useDocument = (collectionName, id) => {
-    const [document, setDocument] = useState(null)
-    const [error, setError] = useState(null)
+export const getDocumentRealTime = (collectionName, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const docRef = doc(firestore, collectionName, id)
+  useEffect(() => {
+    const docRef = doc(firestore, collectionName, id);
 
-        const unsubscribe = onSnapshot(docRef,  (snap) => {
-            if(snap.data()) {
-                setDocument({...snap.data(), id: snap.id})
-                setError(null)
-            } else {
-                setError("Такого проекта нет!!!")
-            }
-        }, (err) => {
-            setError(err)
-          }
-        )
+    const unsubscribe = onSnapshot(
+      docRef,
+      (snap) => {
+        if (snap.data()) {
+          setDocument({ ...snap.data(), id: snap.id });
+          setError(null);
+        } else {
+          setError("Такого проекта нет!!!");
+        }
+      },
+      (err) => {
+        setError(err);
+      }
+    );
 
-        return () => unsubscribe()
-    },[collectionName, id])
+    return () => unsubscribe();
+  }, [collectionName, id]);
 
-    return { document, error }
-}
+  return { document, error };
+};
+
+export const addDocument = async (collectionName, newDoc) => {
+  const [error, setError] = useState(null);
+  const collectionRef = collection(firestore, collectionName);
+
+  try {
+    await addDoc(collectionRef, newDoc);
+    setError(null);
+  } catch (err) {
+    setError(err.message);
+  }
+
+  return { error };
+};
